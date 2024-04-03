e45b0929a64739af59960df2e13ac6008ed1c651
junyub301 <junyub0301@gmail.com>
2024-04-03 22:05:17 +0900

    Feat: firestore mock data추가

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import globalStyles from '@styles/globalStyles'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
 import reportWebVitals from './reportWebVitals'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
