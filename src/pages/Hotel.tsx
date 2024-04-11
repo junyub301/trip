7534784470552f9b8b362ba373d2a9cb7d4c987b
junyub301 <junyub0301@gmail.com>
2024-04-11 22:50:50 +0900

    Feat: hotel-detail markdown

--- src/pages/Hotel.tsx
+++ src/pages/Hotel.tsx
@@ -1,4 +1,5 @@
 import Carousel from '@components/hotel/Carousel'
+import Contents from '@components/hotel/Contents'
 import useHotel from '@components/hotel/hooks/useHotel'
 import Top from '@shared/Top'
 import { useParams } from 'react-router-dom'
@@ -8,11 +9,12 @@ export default function Hotel() {
   const { isLoading, data } = useHotel({ id })
 
   if (data == null || isLoading) return <div>Loading...</div>
-  const { name, comment, images } = data
+  const { name, comment, images, contents } = data
   return (
     <div>
       <Top title={name} subTitle={comment} />
       <Carousel images={images} />
+      <Contents contents={contents} />
     </div>
   )
 }
